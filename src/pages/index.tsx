3032153d7bd49b080e1bf306991c2d0a5055449f
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2023-06-04 10:15:22 +0200

    remove defaultValue property

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -259,7 +259,6 @@ function Modal({
                               {...register("status")}
                               id="status"
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
-                              defaultValue={user?.status}
                             >
                               <option>Approved</option>
                               <option>Pending</option>
